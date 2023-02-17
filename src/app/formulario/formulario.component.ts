0579d1979ab08bbc57d3c32e6b015272dc192f2b
diego.leguizamon <diegog_legui@hotmail.com>
2023-02-17 11:05:30 -0300

    aplicamos referencia local y viewchild

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Persona } from '../personna.model';
 import Swal from 'sweetalert2';
 
@@ -11,10 +11,14 @@ export class FormularioComponent {
   
   @Output() personaCreada = new EventEmitter<Persona>();
   @Output() personaBorrada = new EventEmitter<number>();
-  nombreInput: string = '';
-  apellidoInput: string = '';
+  //nombreInput: string = '';
+  //apellidoInput: string = '';
+  @ViewChild ('nombreRef') nombre: ElementRef;
+  @ViewChild ('apellidoRef') apellido: ElementRef;
   idBorrar:number=0;
 
+  /*
+  usando two way binding
   agregarPersona(){
     if (this.nombreInput=="" || this.apellidoInput==""){
       Swal.fire("No puede ingresar una persona vacia");
@@ -24,6 +28,27 @@ export class FormularioComponent {
       this.personaCreada.emit(persona1);
     }
   }
+  */
+   //usando referencia local
+   /*agregarPersona(nombreRef: HTMLInputElement, apellidoRef:HTMLInputElement){
+    //let persona1 = new Persona(nombreRef.value, apellidoRef.value);
+    
+    if(nombreRef.value=="" || apellidoRef.value==""){
+      Swal.fire("No puede ingresar una persona Vacia");
+    }else{
+      let persona1 = new Persona(nombreRef.value, apellidoRef.value);
+      this.personaCreada.emit(persona1);
+    }
+   }*/
+   //usando VIEWCHILD
+   agregarPersona(){
+    if(this.nombre.nativeElement.value=="" || this.apellido.nativeElement.value==""){
+      Swal.fire("No puede ingresar una persona Vacia");
+    }else{
+      let persona1 = new Persona(this.nombre.nativeElement.value, this.apellido.nativeElement.value);
+      this.personaCreada.emit(persona1);
+    }
+   }
   borrarPersona(){
     //this.personas.splice(this.idBorrar-1,1);
     let indice: number=this.idBorrar;
